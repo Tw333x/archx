3bda426234f7358068dbd83e3344581979045941
virtualdemon <virtual.demon.message@gmail.com>
2018-03-04 23:16:39 +0000

    70% completed

--- auto_bacman.sh
+++ auto_bacman.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+if [[ $( ping -c 3 8.8.8.8 ) ]]
+
+then
+
+    echo  -e "  >> Starting oflline installation \n"
+    sleep 3
+  else
+    echo -e "   >> Internet Connection detected ! exiting..."
+    sleep 2
+    exit -1
+fi
+
 mkdir -p /mnt/var/cache/pacman/pkg/ && cd /mnt/var/cache/pacman/pkg/
 clear
 
@@ -10,7 +22,7 @@ read -p "The whole system packages will be recreate! proceed?(y/n)[n]: " Answer_
 
 case "$Answer_Package_Recreate" in
 	
-	"y"|"Yes"|"YES"|"Y")
+	"y"|"yes"|"Yes"|"YES"|"Y")
 		$( pacman -Qq | awk ' { print $1 } ' |sed  's/^.*\// /g' > PackageList.txt)
 		;;
 
@@ -48,7 +60,7 @@ clear
 # GENERATE FSTAB
 genfstab -U -p /mnt >> /mnt/etc/fstab && cat /mnt/etc/fstab
 
-echo -e '\n Connect internet and execute : curl -s -o chroot_jobs.sh   https://raw.githubusercontent.com/virtualdemon/archx/master/chroot_jobs.sh && chmod +x chroot_jobs.sh && ./chroot_jobs.sh'
+echo -e '\nNow you can connect to internet and execute : curl -s -o chroot_jobs.sh   https://raw.githubusercontent.com/virtualdemon/archx/master/chroot_jobs.sh && chmod +x chroot_jobs.sh && ./chroot_jobs.sh'
 
 # CHROOT TO INSTALLED SYSTEM AND MAKE CUSTOMIZATION
 arch-chroot /mnt 
