302c434169576bf386d7cf99d3e187d245ae8824
virtualdemon <virtual.demon.message@gmail.com>
2018-03-15 12:48:13 +0000

    Added more discription

--- auto_bacman.sh
+++ auto_bacman.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env bash
 
+# Check internet connection : for offline installation
 if [[ ! $( ping -c 3 8.8.8.8 ) ]]; then
 
     echo  -e "  >> Starting oflline installation \n"
     sleep 3
-  else
-    echo -e "   >> Internet Connection detected ! exiting..."
-    sleep 2
-    exit -1
+    else
+      echo -e "   >> Internet Connection detected ! exiting..."
+      sleep 2
+      exit -1
 fi
 
+# Create pacman-cache directory and change directory to there
 mkdir -p /mnt/var/cache/pacman/pkg/ && cd /mnt/var/cache/pacman/pkg/
 clear
 
 echo "You are here : " && pwd
 
-# RECREATE PACKAGES
+# Recreate live system packages with bacman
 read -p "The whole system packages will be recreate! proceed?(y/n)[n]: " Answer_Package_Recreate
 
 case "$Answer_Package_Recreate" in
@@ -32,28 +34,35 @@ case "$Answer_Package_Recreate" in
 		;;
 esac
 
+# Remove corrupted packages
 rm -f *.part
 
+# Change directory to ~/
 cd
 
-# INSTALL PACKAGES TO /MNT
+# Pacstrap packages without installation (just for creating the base system directories...)
 pacstrap /mnt base 
 
+# Create boot directory in new system
 mkdir -m 755 -p /mnt/boot
 
+# Copy vmlinuz-linux from live system  to new system 
 cp -avT /run/archiso/bootmnt/arch/boot/$(uname -m)/vmlinuz /mnt/boot/vmlinuz-linux
 
+# Start Offline package installation to /mnt
 pacman -r /mnt -U /mnt/var/cache/pacman/pkg/* 2> /dev/null
 
+# Change root directory permissions
 chmod 700 /mnt/root
 
 clear
 
-# GENERATE FSTAB
+# Generate FSTAB via devices uuid : you can edit it in /etc/fstab
 genfstab -U -p /mnt >> /mnt/etc/fstab && cat /mnt/etc/fstab
 
+# Manual for using the chroot_jobs.sh in https://github.com/virtualdemon/archx
 echo -e '\nNow you can connect to internet and execute : curl -s -o chroot_jobs.sh   https://raw.githubusercontent.com/virtualdemon/archx/master/chroot_jobs.sh && chmod +x chroot_jobs.sh && ./chroot_jobs.sh'
 
-# CHROOT TO INSTALLED SYSTEM AND MAKE CUSTOMIZATION
+# Change root to the new installed system in /mnt
 arch-chroot /mnt 
 
--- chroot_jobs.sh
+++ chroot_jobs.sh
@@ -4,7 +4,7 @@ export LC_ALL=C
 
 clear
 
-#CHECK THE USER IS root
+# Check if user was root continue the script
 if [[ $( id -u ) != 0 ]]
 
 then
@@ -15,7 +15,7 @@ then
 
 fi
 
-#CHECK INTERNET CONNECTION IS OFFLINE
+# Check the internet connection : you should be ONLINE
 echo -e "\n    >> Checking internet connection...."
 if [[ ! $( ping -c 3 8.8.8.8 ) ]]
 
@@ -33,6 +33,7 @@ pacman-key --init
 pacman-key --populate archlinux
 pacman-key --refresh-keys
 
+# Install tools for bootloader installation
 echo -e "Installing needed tools ! \n"
 if [[ -d "/boot/efi" || -d "/boot/EFI" ]]; then
     echo "  >> EFI partition detected! \n"
@@ -43,15 +44,19 @@ fi
 
 clear
 
+# Set password for root user 
 echo -e "Set a PASSWORD for root user : \n "
 passwd
 
+# Create a normal user
 read -p  "Please enter a USERNAME : " USER_NAME_INPUT
 useradd -m -g users  -G wheel,storage,power  -s $(which zsh) $USER_NAME_INPUT
 
+# Set password for new normal user
 echo -e "Set a PASSWORD for $USER_NAME_INPUT : "
 passwd $USER_NAME_INPUT
 
+# Set timezone to Iran
 echo -e "Setting timezone(Iran) \n"
 rm -f /etc/localtime
 ln -s /usr/share/zoneinfo/Iran /etc/localtime 
@@ -59,19 +64,23 @@ hwclock --systohc --utc
 
 clear
 
+# Set hostname for new system
 echo -e "Setting hostname \n"
 read -p "Please enter youre HOSTNAME : " HOST_NAME_INPUT
 echo $HOST_NAME_INPUT > /etc/hostname
 
+# Set locale for new system
 echo -e "Setting locale \n"
 sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
 locale-gen
 
+# Add permission for wheel users to run commands with sudo
 echo -e "Edit /etc/sudoers (let wheel users to execute sudo!) \n"
 sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' /etc/sudoers
 
 clear
 
+# Install grub bootloader
 echo -e "Start installing BootLoader(grub) \n"
 
 if [ -d "/boot/efi" ]; then
@@ -87,6 +96,8 @@ if [ -d "/boot/efi" ]; then
             grub-install --target=i386-pc --recheck /dev/sda
     fi
   fi
+
+# Make initramfs
 echo -e "Making initramfs \n"
 read -p "Which KERNEL did you installed(linux or linux-zen or linux-hardened)? (Default = linux) : " KERNEL_TYPE_INPUT
 if [[ "$KERNEL_TYPE_INPUT" !=  "linux" &&  ! -z "$KERNEL_TYPE_INPUT" ]]; then
@@ -95,10 +106,11 @@ else
     mkinitcpio -p linux
 fi
 
+# Create grub config
 echo -e "Making GRUB CONFIG \n"
 grub-mkconfig -o /boot/grub/grub.cfg
 
-
+# Add pacaur_installer script in : /opt/user_bin/pacaur_installer
 echo -e "Making pacaur_installer script in /opt/user_bin for install aur: \n"
 mkdir -p /opt/user_bin
 touch /opt/user_bin/pacaur_installer && chmod +x /opt/user_bin/pacaur_installer
